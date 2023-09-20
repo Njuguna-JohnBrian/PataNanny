868e0f38e96dffadb3c2bea17772bc151002d1f2
John Brian <njugunajb96@gmail.com>
2023-09-20 03:24:38 +0300

    SignUpFirstNameInputField

---
+++ lib/screens/signup/components/firstname_input.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:patananny/utils/theme/theme.dart';
+
+class SignUpFirstNameInputField extends StatelessWidget {
+  final TextEditingController textEditingController;
+
+  const SignUpFirstNameInputField({
+    super.key,
+    required this.textEditingController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: EdgeInsets.only(
+        left: size.width * 0.10,
+        right: size.width * 0.10,
+      ),
+      child: TextFormField(
+        controller: textEditingController,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        validator: ((value) {
+          if (value!.isEmpty) {
+            return "First name cannot be empty";
+          } else if (!RegExp("[a-zA-Z]").hasMatch(value)) {
+            return "Please use only letters for your first name";
+          } else if (value.length < 3) {
+            return "Please ensure your first name is 3 characters and above";
+          } else {
+            return null;
+          }
+        }),
+        cursorColor: Colors.grey,
+        keyboardType: TextInputType.name,
+        decoration: InputDecoration(
+          fillColor: Colors.grey.shade200,
+          filled: true,
+          prefixIcon: Icon(
+            Icons.keyboard,
+            color: Colors.grey.shade400,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(
+              10.0,
+            ),
+            borderSide: BorderSide(
+              color: Colors.grey.shade200,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(
+              10.0,
+            ),
+            borderSide: BorderSide(
+              color: Colors.grey.shade200,
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(
+              10.0,
+            ),
+            borderSide: BorderSide(
+              color: Colors.grey.shade200,
+            ),
+          ),
+          labelText: "First Name",
+          hintText: "Frida",
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          floatingLabelStyle: NannyTheme.lightTextTheme.headline6,
+          labelStyle: NannyTheme.lightTextTheme.headline6?.copyWith(
+            color: Colors.grey.shade500,
+          ),
+        ),
+      ),
+    );
+  }
+}
